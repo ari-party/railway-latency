1bc0d4a1ae64e86f0e2d8bc905f4ff153b1dfdd6
astrid <git@astrid.email>
2025-11-11 21:50:40 +0100

    forgot port

--- apps/frontend/src/server/api/trpc/routers/data/index.ts
+++ apps/frontend/src/server/api/trpc/routers/data/index.ts
@@ -9,7 +9,7 @@ let data: Record<string, Record<string, number | null>> = {};
 setIntervalAsync(async () => {
   try {
     const response = await ky
-      .get(`http://${env.MANAGER_HOST}/query/last`)
+      .get(`http://${env.MANAGER_HOST}:8080/query/last`)
       .json();
     data = response as typeof data;
   } catch (err) {
