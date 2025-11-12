a4d54228c2e7aa527defabba69073d1c76db9c3f
astrid <git@astrid.email>
2025-11-12 01:37:27 +0100

    InfluxDB flush on shutdown

--- apps/aggregator/src/influxdb.ts
+++ apps/aggregator/src/influxdb.ts
@@ -1,6 +1,7 @@
 import { InfluxDB } from '@influxdata/influxdb-client';
 
 import { env } from '@/env';
+import { log } from '@/pino';
 
 export const influxDB = new InfluxDB({
   url: env.INFLUXDB_URL,
@@ -12,3 +13,21 @@ export const writeAPI = influxDB.getWriteApi(
   env.INFLUXDB_ORG,
   env.INFLUXDB_BUCKET,
 );
+
+let isShuttingDown = false;
+async function onShutdown() {
+  if (isShuttingDown) return;
+  isShuttingDown = true;
+
+  await writeAPI
+    .close()
+    .then(() => log.info({ name: 'InfluxDB' }, 'Closed write API'))
+    .catch((error) =>
+      log.error({ name: 'InfluxDB', ...error }, 'Failed to close write API'),
+    );
+
+  process.exit(0);
+}
+
+const signals = ['SIGINT', 'SIGTERM'];
+for (const signal of signals) process.on(signal, onShutdown);
