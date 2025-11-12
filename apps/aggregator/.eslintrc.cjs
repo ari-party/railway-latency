6368a44c3525e0b9a7959300fb08e093e8f4b55b
astrid <git@astrid.email>
2025-11-12 18:40:28 +0100

    import/no-extraneous-dependencies

--- apps/aggregator/.eslintrc.cjs
+++ apps/aggregator/.eslintrc.cjs
@@ -1,2 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+// eslint-disable-next-line @typescript-eslint/no-require-imports, import/no-extraneous-dependencies
 module.exports = require('@railway-latency/eslint-config')(__dirname);
