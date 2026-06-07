225ffddedf352c47d2d15560e223b5035af8c08d
astrid <git@astrid.email>
2026-06-07 18:56:59 +0200

    fix(probe): show debug logs for DEBUG_TARGET_REGIONS

--- apps/probe/src/log.rs
+++ apps/probe/src/log.rs
@@ -1,8 +1,15 @@
 use std::io;
 
+use tracing_subscriber::EnvFilter;
+
 pub fn init() {
+  let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_|
+    EnvFilter::new("info,probe=debug")
+  );
+
   tracing_subscriber
     ::fmt()
+    .with_env_filter(filter)
     .event_format(tracing_subscriber::fmt::format().json().flatten_event(true))
     .with_writer(io::stdout)
     .with_target(false)
