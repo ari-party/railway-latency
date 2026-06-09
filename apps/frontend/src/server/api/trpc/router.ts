3c27f9980e6fc30c0e44b78d3833dd975fa84bef
astrid <git@astrid.email>
2026-06-09 04:21:25 +0200

    Add alerts tRPC router

--- apps/frontend/src/server/api/trpc/router.ts
+++ apps/frontend/src/server/api/trpc/router.ts
@@ -1,3 +1,4 @@
+import { alertsRouter } from '@/server/api/trpc/routers/alerts';
 import { chartRouter } from '@/server/api/trpc/routers/chart';
 import { regionsRouter } from '@/server/api/trpc/routers/regions';
 import { tableRouter } from '@/server/api/trpc/routers/table';
@@ -5,6 +6,7 @@ import { tableRouter } from '@/server/api/trpc/routers/table';
 import { createTRPCRouter } from './context';
 
 export const appRouter = createTRPCRouter({
+  alerts: alertsRouter,
   chart: chartRouter,
   regions: regionsRouter,
   table: tableRouter,
