eb827bd02d6e69299c300105a592a368cfc4ba6f
astrid <git@astrid.email>
2026-06-19 21:44:24 +0200

    chore: remove decommissioned influx package

--- packages/influx/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { EXTERNAL_MEASUREMENTS, MEASUREMENT_INFO } from '@/measurements';
-export { buildErrorPoint, buildMtrPoint, buildSamplePoint } from '@/points';
-export { createWriteApi } from '@/writeApi';
-export type { WriteApiConfig } from '@/writeApi';
--- packages/influx/tsup.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/**/*.ts', '!src/**/*.test.ts'],
-  format: 'esm',
-  dts: true,
-});
