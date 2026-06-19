98fac15b1daad8f694e2f8237b38364b083ab8b5
astrid <git@astrid.email>
2026-06-19 21:44:17 +0200

    refactor(frontend): remove dead alerts feature

--- apps/frontend/src/server/api/trpc/router.ts
+++ apps/frontend/src/server/api/trpc/router.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from '@/server/api/trpc/context';
-import { alertsRouter } from '@/server/api/trpc/routers/alerts';
 import { chartRouter } from '@/server/api/trpc/routers/chart';
 import { checksRouter } from '@/server/api/trpc/routers/checks';
 import { mtrRouter } from '@/server/api/trpc/routers/mtr';
@@ -8,7 +7,6 @@ import { regionsRouter } from '@/server/api/trpc/routers/regions';
 import { tableRouter } from '@/server/api/trpc/routers/table';
 
 export const appRouter = createTRPCRouter({
-  alerts: alertsRouter,
   chart: chartRouter,
   checks: checksRouter,
   mtr: mtrRouter,
