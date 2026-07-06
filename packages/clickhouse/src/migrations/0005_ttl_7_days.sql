8d18ae9b10c8870102f3e75499d58e70246b51e7
astrid <git@astrid.email>
2026-07-07 01:49:48 +0200

    fix(clickhouse): avoid TTL materialization blocking on migrate

--- packages/clickhouse/src/migrations/0005_ttl_7_days.sql
+++ packages/clickhouse/src/migrations/0005_ttl_7_days.sql
@@ -1,4 +1,4 @@
-ALTER TABLE check_events MODIFY TTL toDateTime(time) + INTERVAL 7 DAY;
-ALTER TABLE samples MODIFY TTL toDateTime(time) + INTERVAL 7 DAY;
-ALTER TABLE error_events MODIFY TTL toDateTime(time) + INTERVAL 7 DAY;
-ALTER TABLE mtr_events MODIFY TTL toDateTime(time) + INTERVAL 7 DAY;
+ALTER TABLE check_events MODIFY TTL toDateTime(time) + INTERVAL 7 DAY SETTINGS materialize_ttl_after_modify = 0;
+ALTER TABLE samples MODIFY TTL toDateTime(time) + INTERVAL 7 DAY SETTINGS materialize_ttl_after_modify = 0;
+ALTER TABLE error_events MODIFY TTL toDateTime(time) + INTERVAL 7 DAY SETTINGS materialize_ttl_after_modify = 0;
+ALTER TABLE mtr_events MODIFY TTL toDateTime(time) + INTERVAL 7 DAY SETTINGS materialize_ttl_after_modify = 0;
